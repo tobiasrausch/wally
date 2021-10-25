0e8dd4ab8e4868ed992e7aa237386639a833a45b
tobiasrausch <rauschtobi@gmail.com>
2021-10-25 17:26:02 +0200

    cigar

--- src/region.h
+++ src/region.h
@@ -57,6 +57,15 @@ namespace wallysworld
     // Tracks
     int32_t maxTracks = c.height / c.tlheight;
     std::vector<int32_t> taken(maxTracks, -2000000);
+    if (maxTracks < 10) {
+      std::cerr << "Image height is too small!" << std::endl;
+      return 1;
+    }
+    // First 3 tracks: coverage
+    taken[0] = c.width;
+    taken[1] = c.width;
+    taken[2] = c.width;
+
     
     // Open file handles
     typedef std::vector<samFile*> TSamFile;
@@ -73,7 +82,6 @@ namespace wallysworld
     // Parse BAM files
     boost::posix_time::ptime now = boost::posix_time::second_clock::local_time();
     std::cout << '[' << boost::posix_time::to_simple_string(now) << "] " << "Parsing BAMs" << std::endl;
-    faidx_t* fai = fai_load(c.genome.string().c_str());
 
     // Split region
     uint32_t rgbeg = 17383;
@@ -88,10 +96,26 @@ namespace wallysworld
       std::cerr << "Chromosome does not exist in BAM file: " << chrName << std::endl;
       return 1;
     }
+    if (rgbeg >= rgend) {
+      std::cerr << "Region begin needs to be smaller than region end: " << rgbeg << ',' << rgend << std::endl;
+      return 1;
+    }
+    if (rgend - rgbeg > 50000) {
+      std::cerr << "Region needs to be smaller than 50kbp!" << std::endl;
+      return 1;
+    }
 
+    // Load genome
+    faidx_t* fai = fai_load(c.genome.string().c_str());
+    int32_t seqlen;
+    char* seq = faidx_fetch_seq(fai, chrName.c_str(), 0, hdr->target_len[tid], &seqlen);
+    
+    // Coverage
+    uint32_t rgsize = rgend - rgbeg;
+    uint32_t maxCoverage = std::numeric_limits<uint32_t>::max();
+    std::vector<uint32_t> cov(rgsize, 0);
 	
     // Iterate files
-    int32_t rgsize = rgend - rgbeg;
     for(unsigned int file_c = 0; file_c < c.files.size(); ++file_c) {
       // Read alignments
       hts_itr_t* iter = sam_itr_queryi(idx[file_c], tid, rgbeg, rgend);	
@@ -100,8 +124,14 @@ namespace wallysworld
 	if (rec->core.flag & (BAM_FQCFAIL | BAM_FDUP | BAM_FUNMAP | BAM_FSECONDARY | BAM_FSUPPLEMENTARY)) continue;
 	if ((rec->core.qual < c.minMapQual) || (rec->core.tid<0)) continue;
 
-	/*
-	uint32_t rp = 0; // reference pointer
+	// Load sequence
+	std::string sequence;
+	sequence.resize(rec->core.l_qseq);
+	uint8_t* seqptr = bam_get_seq(rec);
+	for (int i = 0; i < rec->core.l_qseq; ++i) sequence[i] = "=ACMGRSVTWYHKDBN"[bam_seqi(seqptr, i)];
+
+	// Parse CIGAR
+	uint32_t rp = rec->core.pos; // reference pointer
 	uint32_t sp = 0; // sequence pointer
 	// Parse the CIGAR
 	uint32_t* cigar = bam_get_cigar(rec);
@@ -109,53 +139,35 @@ namespace wallysworld
 	  if ((bam_cigar_op(cigar[i]) == BAM_CMATCH) || (bam_cigar_op(cigar[i]) == BAM_CEQUAL) || (bam_cigar_op(cigar[i]) == BAM_CDIFF)) {
 	    // match or mismatch
 	    for(std::size_t k = 0; k<bam_cigar_oplen(cigar[i]);++k) {
+	      // Increase coverage
+	      int32_t rpadj = (int32_t) rp - (int32_t) rgbeg;
+	      if ((rpadj >= 0) && (rpadj < (int32_t) rgsize) && cov[rpadj] < maxCoverage) ++cov[rpadj];
 	      if (rec->core.l_qseq) {
-		if (sequence[sp] == refslice[rp]) ++itRg->second.bc.matchCount;
-	      } else {
-		if (bam_cigar_op(cigar[i]) == BAM_CEQUAL) ++itRg->second.bc.matchCount;
-		else if (bam_cigar_op(cigar[i]) == BAM_CDIFF) ++itRg->second.bc.mismatchCount;
+		if (sequence[sp] != seq[rp]) std::cout << rp << std::endl;
 	      }
-	      // Count bp-level coverage
-	      if (itRg->second.bc.cov[rec->core.pos + rp] < itRg->second.bc.maxCoverage) ++itRg->second.bc.cov[rec->core.pos + rp];
 	      ++sp;
 	      ++rp;
 	    }
 	  } else if (bam_cigar_op(cigar[i]) == BAM_CDEL) {
-	    ++itRg->second.bc.delCount;
-	    if (rec->core.l_qseq) ++itRg->second.bc.delHomACGTN[homopolymerContext(sequence, sp, 3)];
-	    if (bam_cigar_oplen(cigar[i]) < itRg->second.bc.maxIndelSize) ++itRg->second.bc.delSize[bam_cigar_oplen(cigar[i])];
-	    else ++itRg->second.bc.delSize[itRg->second.bc.maxIndelSize];
+	    //if (rec->core.l_qseq) ++itRg->second.bc.delHomACGTN[homopolymerContext(sequence, sp, 3)];
+	    //if (bam_cigar_oplen(cigar[i]) < itRg->second.bc.maxIndelSize) ++itRg->second.bc.delSize[bam_cigar_oplen(cigar[i])];
+	    //else ++itRg->second.bc.delSize[itRg->second.bc.maxIndelSize];
 	    rp += bam_cigar_oplen(cigar[i]);
 	  } else if (bam_cigar_op(cigar[i]) == BAM_CINS) {
-	    ++itRg->second.bc.insCount;
-	    if (rec->core.l_qseq) ++itRg->second.bc.insHomACGTN[homopolymerContext(sequence, sp, 3)];
-	    if (bam_cigar_oplen(cigar[i]) < itRg->second.bc.maxIndelSize) ++itRg->second.bc.insSize[bam_cigar_oplen(cigar[i])];
-	    else ++itRg->second.bc.insSize[itRg->second.bc.maxIndelSize];
+	    //if (rec->core.l_qseq) ++itRg->second.bc.insHomACGTN[homopolymerContext(sequence, sp, 3)];
+	    //if (bam_cigar_oplen(cigar[i]) < itRg->second.bc.maxIndelSize) ++itRg->second.bc.insSize[bam_cigar_oplen(cigar[i])];
+	    //else ++itRg->second.bc.insSize[itRg->second.bc.maxIndelSize];
 	    sp += bam_cigar_oplen(cigar[i]);
 	  } else if (bam_cigar_op(cigar[i]) == BAM_CSOFT_CLIP) {
-	    if (!softClippedOnce) {
-	      ++itRg->second.bc.softClipCount;
-	      softClippedOnce = true;
-	    }
 	    sp += bam_cigar_oplen(cigar[i]);
 	  } else if(bam_cigar_op(cigar[i]) == BAM_CHARD_CLIP) {
-	    if (!hardClippedOnce) {
-	      ++itRg->second.bc.hardClipCount;
-	      hardClippedOnce = true;
-	    }
 	  } else if (bam_cigar_op(cigar[i]) == BAM_CREF_SKIP) {
-	    if (!spliced) {
-	      ++itRg->second.rc.spliced;
-	      spliced = true;
-	    }
 	    rp += bam_cigar_oplen(cigar[i]);
 	  } else {
 	    std::cerr << "Unknown Cigar options" << std::endl;
 	    return 1;
 	  }
 	}
-	*/
-	
 
 	int32_t px = pixelX(c.width, rgsize, (rec->core.pos - rgbeg));
 	int32_t pxend = pixelX(c.width, rgsize, (rec->core.pos + alignmentLength(rec) - rgbeg));
@@ -174,6 +186,7 @@ namespace wallysworld
     }
 
     // Clean-up
+    if (seq != NULL) free(seq);
     fai_destroy(fai);
     bam_hdr_destroy(hdr);
     for(unsigned int file_c = 0; file_c < c.files.size(); ++file_c) {
@@ -184,7 +197,7 @@ namespace wallysworld
 
     std::string str("title");
     cv::imwrite("bg.jpg", bg);
-    //cv::imshow(str.c_str(), bg);
+    cv::imshow(str.c_str(), bg);
     cv::waitKey(0);
 
 #ifdef PROFILE
