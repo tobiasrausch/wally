4171d62263881ae0af33893faf908e5c4282f0bd
tobiasrausch <rauschtobi@gmail.com>
2021-10-28 13:13:30 +0200

    region vector

--- src/region.h
+++ src/region.h
@@ -54,24 +54,6 @@ namespace wallysworld
 #ifdef PROFILE
     ProfilerStart("wally.prof");
 #endif
-
-    cv::Mat bg( c.height, c.width, CV_8UC3, cv::Scalar(255, 255, 255));
-
-    // Tracks
-    int32_t maxTracks = c.height / c.tlheight;
-    std::vector<int32_t> taken(maxTracks, -2000000);
-    if (maxTracks < 10) {
-      std::cerr << "Image height is too small!" << std::endl;
-      return 1;
-    }
-    // First 3 tracks: coverage
-    taken[0] = 1073741824;
-    taken[1] = 1073741824;
-    taken[2] = 1073741824;
-    taken[3] = 1073741824;
-    taken[4] = 1073741824;
-    taken[5] = 1073741824;
-    
     // Open file handles
     typedef std::vector<samFile*> TSamFile;
     typedef std::vector<hts_idx_t*> TIndex;
@@ -84,164 +66,198 @@ namespace wallysworld
     }
     bam_hdr_t* hdr = sam_hdr_read(samfile[0]);
 
-    // Parse BAM files
-    boost::posix_time::ptime now = boost::posix_time::second_clock::local_time();
-    std::cout << '[' << boost::posix_time::to_simple_string(now) << "] " << "Parsing BAMs" << std::endl;
-
-    // Split region
-    Region rg;
-    if (!parseRegion(hdr, c.regionStr, rg)) return 1;
-    c.pxoffset = (1.0 / (double) rg.size) * (double) c.width;
-
     // Load genome
     faidx_t* fai = fai_load(c.genome.string().c_str());
     int32_t seqlen;
-    char* seq = faidx_fetch_seq(fai, hdr->target_name[rg.tid], 0, hdr->target_len[rg.tid], &seqlen);
+    std::string oldchr("None");
+    char* seq = NULL;
     
-    // Header
-    drawGenome(c, rg, bg, 1);
+    // Split region
+    std::vector<Region> rg(1);
+    Region tmp;
+    parseRegion(hdr, c.regionStr, tmp);
+    rg[0].tid = tmp.tid;
+    rg[0].beg = tmp.beg;
+    rg[0].end = tmp.end;
+    rg[0].size = tmp.size;
+    for(uint32_t rgIdx = 0; rgIdx < rg.size(); ++rgIdx) {
+      // Get pixel width of 1bp
+      c.pxoffset = (1.0 / (double) rg[rgIdx].size) * (double) c.width;
 
-    // Reference
-    drawReference(c, rg, bg, boost::to_upper_copy(std::string(seq + rg.beg, seq + rg.end)), 2);
-    
-    // Coverage
-    uint32_t maxCoverage = std::numeric_limits<uint16_t>::max();
-    std::vector<uint16_t> covA(rg.size, 0);
-    std::vector<uint16_t> covC(rg.size, 0);
-    std::vector<uint16_t> covG(rg.size, 0);
-    std::vector<uint16_t> covT(rg.size, 0);
+      // Generate image
+      cv::Mat bg( c.height, c.width, CV_8UC3, cv::Scalar(255, 255, 255));
 
-    // Read offset
-    int32_t genomicReadOffset = 0.05 * rg.size;
-    if (genomicReadOffset > 5) genomicReadOffset = 5;
-    else if (genomicReadOffset < 1) genomicReadOffset = 1;
-    
-    // Iterate files
-    for(unsigned int file_c = 0; file_c < c.files.size(); ++file_c) {
-      // Read alignments
-      hts_itr_t* iter = sam_itr_queryi(idx[file_c], rg.tid, rg.beg, rg.end);	
-      bam1_t* rec = bam_init1();
-      while (sam_itr_next(samfile[file_c], iter, rec) >= 0) {
-	if (rec->core.flag & (BAM_FQCFAIL | BAM_FDUP | BAM_FUNMAP | BAM_FSECONDARY | BAM_FSUPPLEMENTARY)) continue;
-	if ((rec->core.qual < c.minMapQual) || (rec->core.tid<0)) continue;
-
-	// Load sequence
-	std::string sequence;
-	sequence.resize(rec->core.l_qseq);
-	uint8_t* seqptr = bam_get_seq(rec);
-	for (int i = 0; i < rec->core.l_qseq; ++i) sequence[i] = "=ACMGRSVTWYHKDBN"[bam_seqi(seqptr, i)];
-
-	// Search empty track
-	int32_t trackIdx = -1;
-	for(uint32_t i = 0; i < taken.size(); ++i) {
-	  if (taken[i] < rec->core.pos) {
-	    trackIdx = i;
-	    taken[i] = rec->core.pos + alignmentLength(rec) + genomicReadOffset;
-	    break;
-	  }
-	}
-	if (trackIdx == -1) continue;
-	
-	// Parse CIGAR
-	uint32_t alnend = rec->core.pos + alignmentLength(rec);
-	uint32_t rp = rec->core.pos; // reference pointer
-	uint32_t sp = 0; // sequence pointer
-	bool firstBox = true;
-	// Parse the CIGAR
-	uint32_t* cigar = bam_get_cigar(rec);
-	for (std::size_t i = 0; i < rec->core.n_cigar; ++i) {
-	  if ((bam_cigar_op(cigar[i]) == BAM_CMATCH) || (bam_cigar_op(cigar[i]) == BAM_CEQUAL) || (bam_cigar_op(cigar[i]) == BAM_CDIFF)) {
-	    bool drawTriangle = false;
-	    if (firstBox) {
-	      if (rec->core.flag & BAM_FREVERSE) drawTriangle = true;
-	      firstBox = false;
-	    }
-	    if ((rp + bam_cigar_oplen(cigar[i]) == alnend) && (!(rec->core.flag & BAM_FREVERSE))) drawTriangle = true;
-	    drawRead(c, rg, bg, trackIdx, (rp - rg.beg), (rp + bam_cigar_oplen(cigar[i]) - rg.beg), (rec->core.flag & BAM_FREVERSE), drawTriangle);
-	    // match or mismatch
-	    for(std::size_t k = 0; k<bam_cigar_oplen(cigar[i]);++k) {
-	      // Increase coverage
-	      int32_t rpadj = (int32_t) rp - (int32_t) rg.beg;
-	      if ((rpadj >= 0) && (rpadj < (int32_t) rg.size)) {
-		if (sequence[sp] == 'A') {
-		  if (covA[rpadj] < maxCoverage) ++covA[rpadj];
-		} else if (sequence[sp] == 'C') {
-		  if (covC[rpadj] < maxCoverage) ++covC[rpadj];
-		} else if (sequence[sp] == 'G') {
-		  if (covG[rpadj] < maxCoverage) ++covG[rpadj];
-		} else if (sequence[sp] == 'T') {
-		  if (covT[rpadj] < maxCoverage) ++covT[rpadj];
-		}
-	      }
-	      // Draw nucleotide for mismatches
-	      if (rec->core.l_qseq) {
-		if (sequence[sp] != seq[rp]) {
-		  drawNuc(c, rg, bg, trackIdx, (rp - rg.beg), (rp + 1 - rg.beg), sequence[sp]);
-		}
-	      }
-	      ++sp;
-	      ++rp;
-	    }
-	  } else if (bam_cigar_op(cigar[i]) == BAM_CDEL) {
-	    drawDel(c, rg, bg, trackIdx, (rp - rg.beg), (rp + bam_cigar_oplen(cigar[i]) - rg.beg), bam_cigar_oplen(cigar[i]));
-	    rp += bam_cigar_oplen(cigar[i]);
-	  } else if (bam_cigar_op(cigar[i]) == BAM_CINS) {
-	    // ToDo
-	    sp += bam_cigar_oplen(cigar[i]);
-	  } else if (bam_cigar_op(cigar[i]) == BAM_CSOFT_CLIP) {
-	    sp += bam_cigar_oplen(cigar[i]);
-	  } else if(bam_cigar_op(cigar[i]) == BAM_CHARD_CLIP) {
-	  } else if (bam_cigar_op(cigar[i]) == BAM_CREF_SKIP) {
-	    rp += bam_cigar_oplen(cigar[i]);
-	  } else {
-	    std::cerr << "Unknown Cigar options" << std::endl;
-	    return 1;
-	  }
-	}
-
+      // Tracks
+      int32_t maxTracks = c.height / c.tlheight;
+      std::vector<int32_t> taken(maxTracks, -2000000);
+      if (maxTracks < 10) {
+	std::cerr << "Image height is too small!" << std::endl;
+	return 1;
       }
-      bam_destroy1(rec);
-      hts_itr_destroy(iter);
+      // Header tracks
+      taken[0] = 1073741824;
+      taken[1] = 1073741824;
+      taken[2] = 1073741824;
+      taken[3] = 1073741824;
+      taken[4] = 1073741824;
+      taken[5] = 1073741824;
+    
+      // Parse BAM files
+      boost::posix_time::ptime now = boost::posix_time::second_clock::local_time();
+      std::cout << '[' << boost::posix_time::to_simple_string(now) << "] " << "Parsing BAMs" << std::endl;
 
-      // Fill-in coverage track
-      std::vector<bool> snp(rg.size, false);
-      for(int32_t rp = rg.beg; rp < rg.end; ++rp) {
-	int32_t rpadj = (int32_t) rp - (int32_t) rg.beg;
-	uint32_t cumsum = covA[rpadj];
-	cumsum += covC[rpadj];
-	cumsum += covG[rpadj];
-	cumsum += covT[rpadj];
-	if (cumsum >= c.snvcov) {
-	  if ((seq[rp] == 'a') || (seq[rp] == 'A')) {
-	    if (((double) covA[rpadj] / (double) cumsum) < (1 - c.snvvaf)) {
-	      snp[rpadj] = true;
+      // Lazy loading of genome
+      std::string chrName = hdr->target_name[rg[rgIdx].tid];
+      if (chrName != oldchr) {
+	if (seq != NULL) free(seq);
+	seq = faidx_fetch_seq(fai, hdr->target_name[rg[rgIdx].tid], 0, hdr->target_len[rg[rgIdx].tid], &seqlen);
+	oldchr = chrName;
+      }
+
+      // Header
+      drawGenome(c, rg[rgIdx], bg, 1);
+
+      // Reference
+      drawReference(c, rg[rgIdx], bg, boost::to_upper_copy(std::string(seq + rg[rgIdx].beg, seq + rg[rgIdx].end)), 2);
+    
+      // Coverage
+      uint32_t maxCoverage = std::numeric_limits<uint16_t>::max();
+      std::vector<uint16_t> covA(rg[rgIdx].size, 0);
+      std::vector<uint16_t> covC(rg[rgIdx].size, 0);
+      std::vector<uint16_t> covG(rg[rgIdx].size, 0);
+      std::vector<uint16_t> covT(rg[rgIdx].size, 0);
+
+      // Read offset
+      int32_t genomicReadOffset = 0.05 * rg[rgIdx].size;
+      if (genomicReadOffset > 5) genomicReadOffset = 5;
+      else if (genomicReadOffset < 1) genomicReadOffset = 1;
+      
+      // Iterate files
+      for(unsigned int file_c = 0; file_c < c.files.size(); ++file_c) {
+	// Read alignments
+	hts_itr_t* iter = sam_itr_queryi(idx[file_c], rg[rgIdx].tid, rg[rgIdx].beg, rg[rgIdx].end);	
+	bam1_t* rec = bam_init1();
+	while (sam_itr_next(samfile[file_c], iter, rec) >= 0) {
+	  if (rec->core.flag & (BAM_FQCFAIL | BAM_FDUP | BAM_FUNMAP | BAM_FSECONDARY | BAM_FSUPPLEMENTARY)) continue;
+	  if ((rec->core.qual < c.minMapQual) || (rec->core.tid<0)) continue;
+	  
+	  // Load sequence
+	  std::string sequence;
+	  sequence.resize(rec->core.l_qseq);
+	  uint8_t* seqptr = bam_get_seq(rec);
+	  for (int i = 0; i < rec->core.l_qseq; ++i) sequence[i] = "=ACMGRSVTWYHKDBN"[bam_seqi(seqptr, i)];
+	  
+	  // Search empty track
+	  int32_t trackIdx = -1;
+	  for(uint32_t i = 0; i < taken.size(); ++i) {
+	    if (taken[i] < rec->core.pos) {
+	      trackIdx = i;
+	      taken[i] = rec->core.pos + alignmentLength(rec) + genomicReadOffset;
+	      break;
 	    }
-	  } else if ((seq[rp] == 'c') || (seq[rp] == 'C')) {
-	    if (((double) covC[rpadj] / (double) cumsum) < (1 - c.snvvaf)) {
-	      snp[rpadj] = true;
-	    }
-	  } else if ((seq[rp] == 'g') || (seq[rp] == 'G')) {
-	    if (((double) covG[rpadj] / (double) cumsum) < (1 - c.snvvaf)) {
-	      snp[rpadj] = true;
-	    }
-	  } else if ((seq[rp] == 't') || (seq[rp] == 'T')) {
-	    if (((double) covT[rpadj] / (double) cumsum) < (1 - c.snvvaf)) {
-	      snp[rpadj] = true;
+	  }
+	  if (trackIdx == -1) continue;
+	  
+	  // Parse CIGAR
+	  uint32_t alnend = rec->core.pos + alignmentLength(rec);
+	  uint32_t rp = rec->core.pos; // reference pointer
+	  uint32_t sp = 0; // sequence pointer
+	  bool firstBox = true;
+	  // Parse the CIGAR
+	  uint32_t* cigar = bam_get_cigar(rec);
+	  for (std::size_t i = 0; i < rec->core.n_cigar; ++i) {
+	    if ((bam_cigar_op(cigar[i]) == BAM_CMATCH) || (bam_cigar_op(cigar[i]) == BAM_CEQUAL) || (bam_cigar_op(cigar[i]) == BAM_CDIFF)) {
+	      bool drawTriangle = false;
+	      if (firstBox) {
+		if (rec->core.flag & BAM_FREVERSE) drawTriangle = true;
+		firstBox = false;
+	      }
+	      if ((rp + bam_cigar_oplen(cigar[i]) == alnend) && (!(rec->core.flag & BAM_FREVERSE))) drawTriangle = true;
+	      drawRead(c, rg[rgIdx], bg, trackIdx, (rp - rg[rgIdx].beg), (rp + bam_cigar_oplen(cigar[i]) - rg[rgIdx].beg), (rec->core.flag & BAM_FREVERSE), drawTriangle);
+	      // match or mismatch
+	      for(std::size_t k = 0; k<bam_cigar_oplen(cigar[i]);++k) {
+		// Increase coverage
+		int32_t rpadj = (int32_t) rp - (int32_t) rg[rgIdx].beg;
+		if ((rpadj >= 0) && (rpadj < (int32_t) rg[rgIdx].size)) {
+		  if (sequence[sp] == 'A') {
+		    if (covA[rpadj] < maxCoverage) ++covA[rpadj];
+		  } else if (sequence[sp] == 'C') {
+		    if (covC[rpadj] < maxCoverage) ++covC[rpadj];
+		  } else if (sequence[sp] == 'G') {
+		    if (covG[rpadj] < maxCoverage) ++covG[rpadj];
+		  } else if (sequence[sp] == 'T') {
+		    if (covT[rpadj] < maxCoverage) ++covT[rpadj];
+		  }
+		}
+		// Draw nucleotide for mismatches
+		if (rec->core.l_qseq) {
+		  if (sequence[sp] != seq[rp]) {
+		    drawNuc(c, rg[rgIdx], bg, trackIdx, (rp - rg[rgIdx].beg), (rp + 1 - rg[rgIdx].beg), sequence[sp]);
+		  }
+		}
+		++sp;
+		++rp;
+	      }
+	    } else if (bam_cigar_op(cigar[i]) == BAM_CDEL) {
+	      drawDel(c, rg[rgIdx], bg, trackIdx, (rp - rg[rgIdx].beg), (rp + bam_cigar_oplen(cigar[i]) - rg[rgIdx].beg), bam_cigar_oplen(cigar[i]));
+	      rp += bam_cigar_oplen(cigar[i]);
+	    } else if (bam_cigar_op(cigar[i]) == BAM_CINS) {
+	      // ToDo
+	      sp += bam_cigar_oplen(cigar[i]);
+	    } else if (bam_cigar_op(cigar[i]) == BAM_CSOFT_CLIP) {
+	      sp += bam_cigar_oplen(cigar[i]);
+	    } else if(bam_cigar_op(cigar[i]) == BAM_CHARD_CLIP) {
+	    } else if (bam_cigar_op(cigar[i]) == BAM_CREF_SKIP) {
+	      rp += bam_cigar_oplen(cigar[i]);
+	    } else {
+	      std::cerr << "Unknown Cigar options" << std::endl;
+	      return 1;
 	    }
 	  }
 	}
-      }
-      drawCoverage(c, rg, bg, covA, covC, covG, covT, snp, 4);
-    }
+	bam_destroy1(rec);
+	hts_itr_destroy(iter);
 
-    // Store image
-    cv::imwrite(c.outfile.string().c_str(), bg);
-    if (c.showWindow) {
-      std::string str = hdr->target_name[rg.tid];
-      str += ":" + boost::lexical_cast<std::string>(rg.beg + 1);
-      str += "-" + boost::lexical_cast<std::string>(rg.end + 1);
-      cv::imshow(str.c_str(), bg);
-      cv::waitKey(0);
+	// Fill-in coverage track
+	std::vector<bool> snp(rg[rgIdx].size, false);
+	for(int32_t rp = rg[rgIdx].beg; rp < rg[rgIdx].end; ++rp) {
+	  int32_t rpadj = (int32_t) rp - (int32_t) rg[rgIdx].beg;
+	  uint32_t cumsum = covA[rpadj];
+	  cumsum += covC[rpadj];
+	  cumsum += covG[rpadj];
+	  cumsum += covT[rpadj];
+	  if (cumsum >= c.snvcov) {
+	    if ((seq[rp] == 'a') || (seq[rp] == 'A')) {
+	      if (((double) covA[rpadj] / (double) cumsum) < (1 - c.snvvaf)) {
+		snp[rpadj] = true;
+	      }
+	    } else if ((seq[rp] == 'c') || (seq[rp] == 'C')) {
+	      if (((double) covC[rpadj] / (double) cumsum) < (1 - c.snvvaf)) {
+		snp[rpadj] = true;
+	      }
+	    } else if ((seq[rp] == 'g') || (seq[rp] == 'G')) {
+	      if (((double) covG[rpadj] / (double) cumsum) < (1 - c.snvvaf)) {
+		snp[rpadj] = true;
+	      }
+	    } else if ((seq[rp] == 't') || (seq[rp] == 'T')) {
+	      if (((double) covT[rpadj] / (double) cumsum) < (1 - c.snvvaf)) {
+		snp[rpadj] = true;
+	      }
+	    }
+	  }
+	}
+	drawCoverage(c, rg[rgIdx], bg, covA, covC, covG, covT, snp, 4);
+      }
+
+      // Store image
+      cv::imwrite(c.outfile.string().c_str(), bg);
+      if (c.showWindow) {
+	std::string str = hdr->target_name[rg[rgIdx].tid];
+	str += ":" + boost::lexical_cast<std::string>(rg[rgIdx].beg + 1);
+	str += "-" + boost::lexical_cast<std::string>(rg[rgIdx].end + 1);
+	cv::imshow(str.c_str(), bg);
+	cv::waitKey(0);
+      }
     }
 
     // Clean-up
@@ -258,7 +274,7 @@ namespace wallysworld
 #endif
   
     // End
-    now = boost::posix_time::second_clock::local_time();
+    boost::posix_time::ptime now = boost::posix_time::second_clock::local_time();
     std::cout << '[' << boost::posix_time::to_simple_string(now) << "] Done." << std::endl;;
     return 0;
   }
